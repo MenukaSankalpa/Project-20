97a32c3d76b3127525c1ad4240f0296612c28baa
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-23 13:27:45 +0530

    updated show data in table

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//validate from input 
 function validateForm(){
     var name = document.getElementById("name").value;
     var age = document.getElementById("age").value;
@@ -35,24 +36,67 @@ function validateForm(){
     return true;
 }
 
-function showData(){
+function showData() {
     var peopleList;
-    if(localStorage.getItem("peopleList") == null){
+    if (localStorage.getItem("peopleList") == null) {
         peopleList = [];
-    }
-    else{
-        people = JSON.parse(localStorage.getItem("peopleList"))
+    } else { 
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
     }
 
     var html = "";
 
-    peopleList.forEach(function (element, index){
+    peopleList.forEach(function (element, index) {
         html += "<tr>";
         html += "<td>" + element.name + "</td>";
         html += "<td>" + element.age + "</td>";
         html += "<td>" + element.address + "</td>";
         html += "<td>" + element.email + "</td>";
-        html += '<td><button onclick="deleteData(' + index + ')" class="btn btn-danger">Delete</button><button onclick="updateData(' + index + ')" class="btn btn-warning m-2">Edit</button></td>';
+        html +=
+            '<td><button onclick="deleteData(' +
+            index +
+            ')" class="btn btn-danger">Delete</button> ' +
+            '<button onclick="editData(' +
+            index +
+            ')" class="btn btn-warning">Edit</button></td>';
         html += "</tr>";
     });
+
+    document.querySelector("#crudTable tbody").innerHTML = html;
+}
+
+//loads all data when document or page loaded 
+document.onload = showData();
+
+// function to add data
+
+function AddData() {
+    //if form validated
+    if(validateForm() == true) {
+        var name = document.getElementById("name").value;
+        var age = document.getElementById("age").value;
+        var address = document.getElementById("address").value;
+        var email = document.getElementById("email").value;
+
+        var peopleList;
+        if (localStorage.getItem("peopleList") == null) {
+            peopleList = [];
+        } else {
+            peopleList = JSON.parse(localStorage.getItem("peopleList"));
+        }
+
+        peopleList.push({
+            name: name,
+            age: age,
+            address: address,
+            email: email,
+        });
+
+        localStorage.setItem("peopleList", JSON.stringify(peopleList));
+        showData();
+        document.getElementById("name").value = "";
+        document.getElementById("age").value = "";
+        document.getElementById("address").value = "";
+        document.getElementById("email").value = "";
+    }
 }
